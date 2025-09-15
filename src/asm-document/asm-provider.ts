d030a4503a966d1e670d2f3094fec9ae66caf8e6
Connor Broyles <5069412+cbrl@users.noreply.github.com>
2025-09-15 17:27:35 -0400

    Clear filesystem watcher when document is closed

--- src/asm-document/asm-provider.ts
+++ src/asm-document/asm-provider.ts
@@ -1,4 +1,4 @@
-import { CancellationToken, Disposable, Uri, EventEmitter, TextDocumentContentProvider, Event, ProviderResult, window, workspace, TextDocument, TabInputText, TabChangeEvent, FileSystemWatcher } from 'vscode';
+import { CancellationToken, Disposable, Uri, EventEmitter, TextDocumentContentProvider, Event, ProviderResult, window, workspace, TextDocument, TabInputText, TabChangeEvent } from 'vscode';
 import { CompileHandler } from './compile-handler';
 import { CompileManager } from '../compile-database';
 import { replaceExtension } from '../utils';
@@ -9,6 +9,7 @@ export class AsmProvider implements TextDocumentContentProvider {
     public static scheme = 'assembly';
 
     private compileHandlers = new UriMap<CompileHandler>();
+	private fileWatchers = new UriMap<Disposable>();
 	private decorators = new UriMap<AsmDecorator>();
 
 	private compileManager: CompileManager;
@@ -21,14 +22,13 @@ export class AsmProvider implements TextDocumentContentProvider {
 		this.compileManager = compileDb;
 
 		const onCloseDisposable = workspace.onDidCloseTextDocument(this.onCloseTextDocument.bind(this));
-
 		const onChangeTabsDisposable = window.tabGroups.onDidChangeTabs(this.onChangeTabs.bind(this));
 
-		this.disposables.push(Disposable.from(
+		this.disposables.push(
 			onCloseDisposable,
 			onChangeTabsDisposable,
 			this.onDidChangeEvent
-		));
+		);
 	}
 
 	// Implements TextDocumentContentProvider.provideTextDocumentContent. This function will be called for the given
@@ -61,7 +61,7 @@ export class AsmProvider implements TextDocumentContentProvider {
 			// Fire a change event for the ASM document when the source file changes.
 			const watcher = workspace.createFileSystemWatcher(srcUri.fsPath);
 			const disposable = watcher.onDidChange(() => this.onDidChangeEvent.fire(asmUri));
-			this.disposables.push(Disposable.from(watcher, disposable));
+			this.fileWatchers.set(asmUri, Disposable.from(watcher, disposable));
 
             this.compileHandlers.set(asmUri, document);
         }
@@ -76,6 +76,7 @@ export class AsmProvider implements TextDocumentContentProvider {
 
     public dispose(): void {
 		this.disposables.forEach(d => d.dispose());
+		this.fileWatchers.forEach(d => d.dispose());
         this.compileHandlers.clear();
     }
 
@@ -83,8 +84,7 @@ export class AsmProvider implements TextDocumentContentProvider {
 		const asmDoc = this.compileHandlers.get(document.uri);
 
 		if (asmDoc) {
-			asmDoc.dispose();
-			this.compileHandlers.delete(document.uri);
+			this.unregisterDocument(document.uri);
 		}
 	}
 
@@ -94,18 +94,28 @@ export class AsmProvider implements TextDocumentContentProvider {
 				continue;
 			}
 
-			const asmDoc = this.compileHandlers.get(tab.input.uri);
-			const decorator = this.decorators.get(tab.input.uri);
+			this.unregisterDocument(tab.input.uri);
+		}
+	}
 
-			if (asmDoc !== undefined) {
-				asmDoc.dispose();
-				this.compileHandlers.delete(tab.input.uri);
-			}
+	private unregisterDocument(uri: Uri): void {
+		const asmDoc = this.compileHandlers.get(uri);
+		const decorator = this.decorators.get(uri);
+		const fileWatcher = this.fileWatchers.get(uri);
 
-			if (decorator !== undefined) {
-				decorator.dispose();
-				this.decorators.delete(tab.input.uri);
-			}
+		if (asmDoc !== undefined) {
+			asmDoc.dispose();
+			this.compileHandlers.delete(uri);
+		}
+
+		if (decorator !== undefined) {
+			decorator.dispose();
+			this.decorators.delete(uri);
+		}
+
+		if (fileWatcher !== undefined) {
+			fileWatcher.dispose();
+			this.fileWatchers.delete(uri);
 		}
 	}
 }
